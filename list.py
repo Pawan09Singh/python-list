5cefa29763fd79bc3f777e217a8bdce6cec956c8
Pawan09Singh <119283159+Pawan09Singh@users.noreply.github.com>
2022-12-06 01:23:14 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,17 @@
+capitals={"usa":"washington DC","India":"New delhi","UTTAR PRADESH":"lUCKNOW"}
+print(capitals["India"])
+print(capitals["UTTAR PRADESH"])
+mydict={"name":"Krishna","branch":"CSE","place":"Indranilnagar"}
+print(mydict["name"])
+print(mydict["branch"])
+print(mydict["place"])
+
+mydict={"game":"chess","dish":"mango","place":"home"}
+print(mydict["game"])
+print(mydict.get("game"))
+mydict["game"]="cricket"
+print(mydict["game"])
+#the rule here is you can use single bracket while using .get only.
+
+
+
